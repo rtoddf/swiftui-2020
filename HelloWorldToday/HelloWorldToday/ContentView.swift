760785b092114065e35a15385965f35fa69005e3
Todd Fleeman <toddfleeman@Todds-MacBook-Pro-2.local>
2020-01-06 12:04:21 -0500

    first udemy project

---
+++ HelloWorldToday/HelloWorldToday/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloWorldToday
+//
+//  Created by Todd Fleeman on 1/6/20.
+//  Copyright © 2020 Todd Fleeman. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
