f499f7536910a200252b8dbd6f28263f629c4739
Todd Fleeman <toddfleeman@Todds-MacBook-Pro-2.local>
2020-01-07 11:18:45 -0500

    finished up card exercise

--- HelloWorldToday/HelloWorldToday/ContentView.swift
+++ HelloWorldToday/HelloWorldToday/ContentView.swift
@@ -3,27 +3,37 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack() {
+            Image("lego-todd")
+                .resizable()
+                .scaledToFit()
+                .clipShape(Circle())
+                .overlay(Circle()
+                    .stroke(Color.white, lineWidth: 2))
+                .shadow(radius: 3  )
+                .frame(width: 150, height: 150, alignment: .center)
             VStack(alignment: .leading) {
                 Text("Todd Fleeman")
                     .font(.title)
-                    .foregroundColor(.gray)
+                    .foregroundColor(.white)
                 Text("http://www/rtodd.net")
                     .font(.subheadline)
-                    .foregroundColor(.gray)
+                    .foregroundColor(.white)
                 HStack() {
                     Image(systemName: "t.square.fill")
-                        .foregroundColor(.purple)
+                        .foregroundColor(.white)
                     
                     Text("@rtoddf")
                         .font(.subheadline)
-                        .foregroundColor(.purple)
-                    .bold()
-                    .italic()
+                        .foregroundColor(.white)
+                        .bold()
+                        .italic()
                 }
             }
         }
-//            .frame(width: 300, height: 240)
-//            .background(Color.gray)
+        .frame(width: 380, height: 200)
+        .background(Color.orange)
+        .cornerRadius(10)
+        .shadow(radius: 3)
     }
 }
 
